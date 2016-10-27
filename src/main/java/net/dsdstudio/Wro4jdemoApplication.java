925ca32845a82d5b0d0a187814849441dfcbc42f
보롱킴 <dsdgun@gmail.com>
2016-10-27 23:35:32 +0900

    first commit

---
+++ src/main/java/net/dsdstudio/Wro4jdemoApplication.java
@@ -0,0 +1,12 @@
+package net.dsdstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wro4jdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wro4jdemoApplication.class, args);
+	}
+}
